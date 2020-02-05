2b03baa21a4434181d2d5674e26b86d390b73440
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-02-05 07:46:55 -0500

    Prepare main for submission

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     if read_from_file:
 
-        filename = 'tests/input/large_set2.txt'
+        filename = 'tests/input/example1.txt'
         c_tuples = c_tuples_from_file(filename)
 
     else:
